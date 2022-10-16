19aeb3f83e96d43a914700316508699e6d4ff41e
minidonut <nycom13@gmail.com>
2022-10-17 05:51:56 +0900

    feat: add ts-factory-code-gen command

---
+++ src/command/ts-lang-service/ts-factory-code-gen.ts
@@ -0,0 +1,56 @@
+import assert from 'assert';
+import ts from 'typescript';
+import { Emacs } from '../../emacs';
+import { isTypescriptMode } from '../../util/predicates';
+import { Env } from '../type';
+import { generateCode } from 'ts-factory-code-generator-generator';
+import { generateFactoryCode } from './factoryCodeGenerator';
+import * as fs from 'fs';
+import { singleFileLogger } from '../../logger/SingleFileLogger';
+import parserTypescript from 'prettier/parser-typescript';
+import prettier from 'prettier/standalone';
+
+export const tsFactoryCodeGen = async (target: string, env?: Env) => {
+  assert(env, "Environment required. (use 'post-message-node-with-env')");
+  if (!isTypescriptMode(env)) {
+    throw new Error(`Not a typescript buffer. (${env.mode})`);
+  }
+
+  const sourceFile = ts.createSourceFile(
+    'index.ts',
+    target === 'region' ? env.region : env.buffer,
+    ts.ScriptTarget.Latest
+  );
+
+  const factoryCode = generateFactoryCode(ts, sourceFile);
+  singleFileLogger.log(Emacs.message(factoryCode));
+
+  return Emacs.message(prettify(factoryCode), {
+    name: `*ts-factory-code-gen-${target}*`,
+    on: 'help-window',
+  });
+};
+
+tsFactoryCodeGen.command = 'ts-factory-code-gen';
+
+export function generateFactory() {
+  const code = generateCode('typescript');
+  fs.writeFileSync('./src/command/ts-lang-service/factoryCodeGenerator.ts', code);
+}
+
+function prettify(template: string) {
+  if (!template) {
+    return template;
+  }
+  return prettier.format(template, {
+    parser: 'typescript',
+    plugins: [parserTypescript],
+    printWidth: 100,
+    singleQuote: true,
+    bracketSpacing: true,
+    trailingComma: 'all',
+    arrowParens: 'always',
+    useTabs: false,
+    tabWidth: 2,
+  });
+}
